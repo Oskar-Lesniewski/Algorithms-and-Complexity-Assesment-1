159934ad8b1563d9c7694d40bdbf07208c9f5102
Oskar <Oskarles25@gmail.com>
2023-03-19 20:02:07 +0000

    Error Handling added

--- Algorithms and Complexity Assesment 1/Functions.cs
+++ Algorithms and Complexity Assesment 1/Functions.cs
@@ -71,31 +71,61 @@ namespace Assesment1
         {
             int i = 0;
             int length = Road.Length;
-            for (int x = 0; x < length - 1; x++)
+            while (true)
             {
-                for (int y = 0; y < length - x - 1; y++)
+                try
                 {
-                    if (Road[y] > Road[y + 1])
+                    Console.WriteLine("Ascending (1) or Descending (2) sort?");
+                    int ArrDir = Convert.ToInt32(Console.ReadLine());
+                    if (ArrDir < 1 || ArrDir > 2)
                     {
-                        int temp = Road[y];
-                        Road[y] = Road[y + 1];
-                        Road[y + 1] = temp;
+                        throw new Exception();
+                    }
+                    else
+                    {
+                        string Dir = "Unsorted";
+                        if (ArrDir == 1)
+                        {
+                            Dir = "Ascending";
+                            for (int x = 0; x < length - 1; x++)
+                            {
+                                for (int y = 0; y < length - x - 1; y++)
+                                {
+                                    if (Road[y] > Road[y + 1])
+                                    {
+                                        int Temp = Road[y];
+                                        Road[y] = Road[y + 1];
+                                        Road[y + 1] = Temp;
+                                    }
+                                }
+                            }
+                        }
+                        else if (ArrDir == 2)
+                        {
+                            Dir = "Descending";
+                            for (int x = 0; x < length - 1; x++)
+                            {
+                                for (int y = 0; y < length - x - 1; y++)
+                                {
+                                    if (Road[y] < Road[y + 1])
+                                    {
+                                        int Temp = Road[y];
+                                        Road[y] = Road[y + 1];
+                                        Road[y + 1] = Temp;
+                                    }
+                                }
+                            }
+                        }
+                        Console.WriteLine("The data has been sorted in " + Dir + " order.");
+                        break;
                     }
                 }
-            }
-            Console.WriteLine("Ascending (1) or Descending (2) sort?");
-            int ArrDir = Convert.ToInt32(Console.ReadLine());
-            string Dir = "Unsorted";
-            if (ArrDir == 1)
-            {
-                Dir = "Ascending";
-            }
-            else if (ArrDir == 2)
-            {
-                Dir = "Descending";
-                Array.Reverse(Road);
-            }
-            Console.WriteLine("The data has been sorted in " + Dir + " order.");
+                catch
+                {
+                    Console.WriteLine("Invalid input try again.");
+                    continue;
+                }
+            }        
             for (i = 0; i < Road.Length; i += Increment)
             {
                 if (i == 0)
@@ -108,47 +138,81 @@ namespace Assesment1
                 }
             }
             // Searching
-            Console.WriteLine("Do you want to find a value ihe sorted or unsorted array? 1 or 2 respecively?");
-            int SearchType = Convert.ToInt32(Console.ReadLine());
-            int[] ArrType = { };
-            if (SearchType == 1)
+            while (true)
             {
-                ArrType = Road;
-            }
-            else if (SearchType == 2)
-            {
-                ArrType = RoadUS;
-            }
-            int Max = ArrType.Max();
-            int Min = ArrType.Min();
-            Console.WriteLine("Enter a number you want to find in the data between " + Min + " - " + Max);
-            int Selected = Convert.ToInt32(Console.ReadLine());
-            int FoundValue = -1;
-            int FoundValueIndex = -1;
-            bool WasFound = false;
-            for (int z = 0; z < ArrType.Length; z++)
-            {
-                if (Selected == ArrType[z])
+                try
                 {
-                    FoundValue = ArrType[z];
-                    FoundValueIndex = z + 1;
-                    Console.WriteLine("The value was found at index " + FoundValueIndex + " and the value was " + FoundValue);
-                    WasFound = true;
+                    Console.WriteLine("Do you want to find a value in the Sorted (1) or Unsorted (2) array?");
+                    int SearchType = Convert.ToInt32(Console.ReadLine());
+                    if (SearchType < 1 || SearchType > 2)
+                    {
+                        throw new Exception();
+                    }
+                    else
+                    {
+                        int[] ArrType = { };
+                        if (SearchType == 1)
+                        {
+                            ArrType = Road;
+                        }
+                        else if (SearchType == 2)
+                        {
+                            ArrType = RoadUS;
+                        }
+                        int Max = ArrType.Max();
+                        int Min = ArrType.Min();
+                        try
+                        {
+                            Console.WriteLine("Enter a number you want to find in the data between " + Min + " - " + Max);
+                            int Selected = Convert.ToInt32(Console.ReadLine());
+                            if (Selected < Min || Selected > Max)
+                            {
+                                throw new Exception();
+                            }
+                            else
+                            {
+                                int FoundValue = -1;
+                                int FoundValueIndex = -1;
+                                bool WasFound = false;
+                                for (int z = 0; z < ArrType.Length; z++)
+                                {
+                                    if (Selected == ArrType[z])
+                                    {
+                                        FoundValue = ArrType[z];
+                                        FoundValueIndex = z + 1;
+                                        Console.WriteLine("The value was found at position " + FoundValueIndex + " and the value found was " + FoundValue);
+                                        WasFound = true;
+                                        continue;
+                                    }
+                                    else
+                                    {
+                                        FoundValue = -1;
+                                    }
+                                }
+                                int ClosestValue = -1;
+                                int ClosestIndex = -1;
+                                if (FoundValue == -1 && WasFound == false)
+                                {
+                                    ClosestValue = ArrType.OrderBy(v => Math.Abs(v - Selected)).First();
+                                    ClosestIndex = Array.IndexOf(ArrType, ClosestValue);
+                                    Console.WriteLine("The value was not found, but the closest one is " + ClosestValue + " in the position " + (ClosestIndex + 1));
+                                }
+                                break;
+                            }
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Inalid input try again.");
+                            continue;
+                        }
+                    }                       
+                }
+                catch
+                {
+                    Console.WriteLine("Inalid input try again.");
                     continue;
                 }
-                else
-                {
-                    FoundValue = -1;
-                }
-            }
-            int ClosestValue = -1;
-            int ClosestIndex = -1;
-            if (FoundValue == -1 && WasFound == false)
-            {
-                ClosestValue = ArrType.OrderBy(v => Math.Abs(v - Selected)).First();
-                ClosestIndex = Array.IndexOf(ArrType, ClosestValue);
-                Console.WriteLine("The value was not found, but the closest one is " + ClosestValue + " in the index position " + (ClosestIndex + 1));
-            }
+            }            
             return false;
         }
         
--- Algorithms and Complexity Assesment 1/Program.cs
+++ Algorithms and Complexity Assesment 1/Program.cs
@@ -9,36 +9,52 @@ namespace Assesment1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What Road Do You Want To Choose? (1,2,3) for roads 1,2,3 256 values, (4,5,6) for roads 1,2,3 2048 values.");
-            int RoadNr = Convert.ToInt32(Console.ReadLine());
-            if (RoadNr == 1)
+            while (true)
             {
-                Reading.Read(1);
+                try
+                {
+                    Console.WriteLine("What Road Do You Want To Choose? (256 Values Enter 1,2,3 for each respective road.) (2048 Values Enter 4,5,6 for each respective road.)");
+                    int RoadNr = Convert.ToInt32(Console.ReadLine());
+                    if (RoadNr < 0 || RoadNr > 6)
+                    {
+                        throw new Exception();
+                    }
+                    else
+                    {
+                        if (RoadNr == 1)
+                        {
+                            Reading.Read(1);
+                        }
+                        else if (RoadNr == 2)
+                        {
+                            Reading.Read(2);
+                        }
+                        else if (RoadNr == 3)
+                        {
+                            Reading.Read(3);
+                        }
+                        else if (RoadNr == 4)
+                        {
+                            Reading.Read(4);
+                        }
+                        else if (RoadNr == 5)
+                        {
+                            Reading.Read(5);
+                        }
+                        else if (RoadNr == 6)
+                        {
+                            Reading.Read(6);
+                        }
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input try again.");
+                    continue;
+                }
+                break;
             }
-            else if (RoadNr == 2)
-            {
-                Reading.Read(2);
-            }
-            else if (RoadNr == 3)
-            {
-                Reading.Read(3);
-            }
-            else if (RoadNr == 4)
-            {
-                Reading.Read(4);
-            }
-            else if (RoadNr == 5)
-            {
-                Reading.Read(5);
-            }
-            else if (RoadNr == 6)
-            {
-                Reading.Read(6);
-            }
-            else
-            {
-                Console.WriteLine("Invalid Number");
-            }
+            Console.WriteLine("Please press ENTER to exit.");
             Console.ReadLine();
         }
     }
